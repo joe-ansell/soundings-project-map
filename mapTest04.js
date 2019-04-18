5186ec8ce99ad7b1214b36bac9ee9d4122551436
joe-ansell <44704063+joe-ansell@users.noreply.github.com>
2019-04-18 16:42:27 +0100

    Add files via upload

--- mapTest04.js
+++ mapTest04.js
@@ -4,10 +4,35 @@ $( document).ready(function(){
 
 var mapboxAccessToken = 'pk.eyJ1Ijoiam9lLWFuc2VsbCIsImEiOiJjanMwY2lqdDkxZDZzM3ltMXhxanBjczgzIn0.cXtLiX_Attz-zpsupXOyvw';
 var map = L.map('mapid', {
-    minZoom: 10,
-    maxZoom: 16,
+    minZoom: 9,
+    maxZoom: 17,
     maxBounds:[ [51.773986, -0.661993], [51.239167, 0.375363], ], 
 }).setView([51.505, -0.09], 10);
+
+//render london outline and set style
+wholeLondon = L.geoJSON(wholeLondon, {
+    style: function(feature) {
+        return {fillOpacity : 0 , weight : 2, interactive: false};
+    },
+}).addTo(map);
+
+
+//set zoom for depending on what window width size
+function mobileView(){
+  if ($(window).width() < 650) {
+      map.setZoom(9.4);
+  }
+  else {
+      map.setZoom(10);
+  }
+}
+
+$( window ).resize(function() {
+
+  mobileView();
+});
+
+mobileView();
 
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
@@ -24,7 +49,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 
     /*------render markers------*/
 
-
 function onEachFeature(feature, layer) {
     layer.on({
         click: zoomToFeature
@@ -32,12 +56,21 @@ function onEachFeature(feature, layer) {
 }  
 
 function zoomToFeature(e, props) {
+
   // map.fitBounds(e.target.getBounds());
   map.setView(e.latlng, 14);
 
   var showthis = e.target.feature.properties.name;
 
   console.log(showthis);
+
+  //reset project-list name opacity
+  $(".text").css('opacity', '0.6');
+
+  //change opacity project-list name to 1
+  $(".text:contains('" + showthis + "')").css('opacity', '1');
+
+  // setTimeout(function(){ map.invalidateSize()}, 500);
 }
 
 function sortByAlphabeticalOrderOfPropertyName(featureA, featureB) {
@@ -71,7 +104,14 @@ londonProjects.features
     // coordinates (which are stored as [lng, lat]).
     $property.click(function(){
       var latlng = new L.LatLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0], 0);
-      map.setView(latlng, 14);
+      map.setView(latlng, 15);
+
+    //reset project-list name opacity
+    $(".text").css('opacity', '0.6');
+    //add opacit: 1 to selected menu item
+    $(this).css('opacity', '1');
+
+      setTimeout(function(){ map.invalidateSize()}, 500);
     })
     // then we append (add) the $div inside $projectsList
     $projectsList.append($property)
@@ -87,18 +127,38 @@ londonProjects = L.geoJSON(londonProjects, {
     onEachFeature: onEachFeature,
 }).addTo(map);
 
+
+
     /*------hovers------*/       
 
-londonProjects.on("mouseover", function(e) {
+londonProjects.on("mouseover click", function(e) {
       var projectName = (e.layer.feature.properties.name);
       document.getElementsByClassName('project-names')[0].innerHTML = projectName;
-      $('.project-names').animate({bottom: '0px'}).clearQueue();
+      TweenMax.to(".project-names-container", 0.5, {css:{bottom:"0px"},ease:Power2.easeOut});
+      TweenMax.to("#flat-slider", 0.5, {css:{bottom:"-200px"},ease:Power2.easeOut});
+      $('.project-names').css({bottom: '0px'}).clearQueue();
+});
+  
+
+function hideTimeLine() {
+    document.getElementsByClassName('project-names')[0].innerHTML = 'test test'
+    TweenMax.to(".project-names-container", 0.5, {css:{bottom:"-200px"},ease:Power2.easeOut});
+    TweenMax.to("#flat-slider", 0.5, {css:{bottom:"0px"},ease:Power2.easeOut});
+    $('.project-names').css({bottom: '-200px'}).clearQueue();
+};
+
+londonProjects.on("mouseout", function(){
+  hideTimeLine();
 });
 
-londonProjects.on("mouseout", function() {
-      document.getElementsByClassName('project-names')[0].innerHTML = ''
-      $('.project-names').animate({bottom: '-200px'}).clearQueue();
+$('.project-names-close-btn').on("click", function() {
+  hideTimeLine();
 });
+
+wholeLondon.on("click", function() {
+  TweenMax.to(".project-names", 0.5, {css:{bottom:"-200px"},ease:Power2.easeOut});
+});
+
 
 map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
 
@@ -108,10 +168,10 @@ map.attributionControl.addAttribution('Population data &copy; <a href="http://ce
 var valuemin = valuemax = 0;
   $("#flat-slider")
     .slider({ 
-        min: 2008,
+        min: 2007,
         max: 2019, 
         range: true,
-        values: [2008, 2019],
+        values: [2007, 2019],
         slide: function( event, ui ) {
 
           var min = ui.values[0],
@@ -135,30 +195,121 @@ var valuemin = valuemax = 0;
         rest: "label",
     });
 
-/*--------popup list-------*/
-  $( ".project-list-btn" ).click(function(){
-    $(".projects-list-container").animate({bottom: '0px'});
-    $(".project-list-btn").animate({bottom: '-50px'});
-    $(".popup-container").css({display: 'none'});
-  });
-
-var closeList = function(){
-  $(".projects-list-container").animate({bottom: '-100%'});
-    $(".project-list-btn").animate({bottom: '0px'});
-    $(".popup-container").css({display: 'block'});
+  var closeList = function(){
+    TweenMax.to(".projects-list-container", 0.5, {css:{left: '-413px'},ease:Power1.easeOut});
+    TweenMax.to(".map-container, .slider-container, .project-names-container", 0.5, {css:{width:"100%"},ease:Power2.easeOut});
+    document.getElementById("project-list-btn-text").innerHTML = "Open project list";
+    TweenMax.to(".fa-arrow-up", 0.5, {css:{transform:"rotate(0deg)"},ease:Power2.easeOut});
+    console.log('close list');
   };
 
-$( "i.fas.fa-times" ).click(closeList);
+  var closeListSmall = function(){
+    TweenMax.to(".projects-list-container", 0.5, {css:{left: '-100%'},ease:Power1.easeOut});
+    // TweenMax.to(".map-container, .slider-container, .project-names-container", 0.5, {css:{width:"100%"},ease:Power2.easeOut});
+    document.getElementById("project-list-btn-text").innerHTML = "Open project list";
+    TweenMax.to(".fa-arrow-up", 0.5, {css:{transform:"rotate(0deg)"},ease:Power2.easeOut});
+    console.log('close list small');
+  };
+
+  var openList = function(){
+    TweenMax.to(".projects-list-container", 0.5, {css:{left: '0px'},ease:Power1.easeOut});
+    TweenMax.to(".map-container, .slider-container, .project-names-container", 0.5, {css:{width:"calc(100% - 413px)"},ease:Power2.easeOut});
+    document.getElementById("project-list-btn-text").innerHTML = "Close project list";
+    TweenMax.to(".fa-arrow-up", 0.5, {css:{transform:"rotate(-180deg)"},ease:Power2.easeOut});
+    console.log('open list');
+  }
+
+  var openListSmall = function(){
+    TweenMax.to(".projects-list-container", 0.5, {css:{left: '0px'},ease:Power1.easeOut});
+    // TweenMax.to(".map-container, .slider-container, .project-names-container", 0.5, {css:{width:"calc(100% - 700px)"},ease:Power2.easeOut});
+    document.getElementById("project-list-btn-text").innerHTML = "Close project list";
+    TweenMax.to(".fa-arrow-up", 0.5, {css:{transform:"rotate(-180deg)"},ease:Power2.easeOut});
+    console.log('open list small');
+  }
+
+/*--------slide out list operations-------*/
+
+    //functions for menu pull out button
+    $( ".project-list-btn" ).click(function(){
+
+    //close pull out menu for smaller devices
+    if ($( ".project-list-btn" ).hasClass('project-list-open') && ($(window).width() < 1024)){
+      closeListSmall();
+      console.log('closingSmall');
+      $( ".project-list-btn" ).removeClass('project-list-open');
+    }
+
+    //close pull out menu
+    else if ($( ".project-list-btn" ).hasClass('project-list-open')){
+      closeList();
+      console.log('closing');
+      $( ".project-list-btn" ).removeClass('project-list-open');
+    }
+
+    //open pull out menu for smaller devices
+    else if ($(window).width() < 1024){
+      openListSmall();
+      $( ".project-list-btn" ).addClass('project-list-open');
+    }
+
+    //open pull out menu
+    else {
+      openList();
+      $( ".project-list-btn" ).addClass('project-list-open');
+    }
+
+    setTimeout(function(){ map.invalidateSize()}, 500);
+
+  });
+
+  //close menu when clicking on the menu list item
+  $( ".projects-list div" ).click(function(){
+      if ($( ".project-list-btn" ).hasClass('project-list-open') && ($(window).width() < 1024)){
+        closeList();
+        console.log('closing');
+        $( ".project-list-btn" ).removeClass('project-list-open');
+      }
+
+      setTimeout(function(){ map.invalidateSize()}, 500);
+  });
+
+   $( ".close-list-btn" ).click(function(){
+    closeListSmall();
+    setTimeout(function(){ map.invalidateSize()}, 500);
+  });
+
+
+
+  $( ".projects-list div" ).click(function(){
+    if ($(window).width() < 1024){
+      closeListSmall();
+    }
+  });
+
+  // $( ".leaflet-left" ).prependTo('.container');
+
+  // $( ".projects-list div" ).click(function(){
+  //   setTimeout(function(){ map.invalidateSize()}, 500);
+  //   if ($(window).width() < 1024){
+  //     closeListSmall();
+  //   }
+  // });
+
+
+
+// var closeList = function(){
+//     TweenMax.to(".projects-list-container", 0.5, {css:{left: '-413px'},ease:Power1.easeOut});
+//     // TweenMax.to(".project-list-btn", 0.5, {css:{bottom: '0px'},ease:Power1.easeOut});
+//     TweenMax.to(".map-container, .slider-container, .project-names-container", 0.5, {css:{width:"100%"},ease:Power2.easeOut});
+//     $(".popup-container").css({display: 'block'});
+//     document.getElementById("project-list-btn-text").innerHTML = "Open project list";
+//     TweenMax.to(".fa-arrow-up", 0.5, {css:{transform:"rotate(180deg)"},ease:Power2.easeOut});
+//     $( ".project-list-btn" ).addClass('project-list-open');
+//   };
+
+// $( "i.fas.fa-times" ).click(closeList);
 
 $( ".projects-list div" ).addClass('text')
-
-// if(typeof(londonProjects.features) === "undefined"){
-//     console.log('oh nah');
-// }else{
-//     console.log('its lit');
-// }
-
-// console.log(londonProjects._layers);
 
 /*-----------project-list hover-----------*/
 
@@ -167,7 +318,7 @@ $('.projects-list div').mouseover(function(){
 
     londonProjects.eachLayer(function (layer) {  
       if(layer.feature.properties.name == thisProject) {    
-        layer.setStyle({fillOpacity : 0.4 , weight : 10}) 
+        layer.setStyle({weight : 10}) 
       }
     });
 
@@ -178,7 +329,7 @@ $('.projects-list div').mouseout(function(){
 
     londonProjects.eachLayer(function (layer) {  
       if(layer.feature.properties.name == thisProject) {    
-        layer.setStyle({fillOpacity : 0.4 , weight : 2}) 
+        layer.setStyle({weight : 2}) 
       }
     });
 
@@ -207,72 +358,6 @@ function style(feature) {
         fillColor: 'black'
     };
 }
-
-
-// geojsonBoro = L.geoJson(londonBoroughs, {
-//     style: style,
-// }).addTo(map);
-
-// $('.projects-list div').click(function(){
-//   var thisProject = $(this).context.innerHTML;
-
-//     londonProjects.eachLayer(function (layer) {  
-//       if(layer.feature.properties.name == thisProject) {    
-//         map.setView(layer.latlng, 14);
-//       }
-//     });
-
-// });
-
-// londonProjects.on('click', function(e){
-//       map.setView(e.latlng, 14);
-//   });
-
-
-
-
-
-
-/*-----------------bits I might need-----------------*/
-
-    // londonProjects = L.geoJSON(londonProjects, {
-    //     pointToLayer: function (feature, latlng) {
-    //         return L.circleMarker(latlng, circles);
-    //     }
-    // }).addTo(map);
-
-    // $('.project-names').on("click", function(){
-    //     map.removeLayer(Hackney, Haringey, WalthamForest, Newham);
-    // });
-
-    // map.addLayer(londonProjects);
-
-    // console.log(londonProjects);
-
-    // L.geoJSON(londonProjects, {
-    // filter: function(feature, latlng) {
-    //     if (feature.properties.density === 1000 ){
-    //         return L.circleMarker(latlng, circlesDim);
-    //     }
-    // }
-    // }).addTo(map);
-
-    //  londonProjects.on("click", function(e) {
-    //     console.log(e.layer.feature.properties);
-    // });
-
-    // /*---------- Project Name on bottom--------*/
-
-
-    // londonProjects.on("mouseover", function(e) {
-    //     var projectName = (e.layer.feature.properties.name);
-    //      document.getElementsByClassName('project-names')[0].innerHTML = projectName;
-    // });
-
-    // londonProjects.on("mouseout", function() {
-    //      document.getElementsByClassName('project-names')[0].innerHTML = 'Hover over a place'
-    // });
-
 
 });
 
